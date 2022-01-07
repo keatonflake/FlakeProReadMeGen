9706cb223d433ba733d3b5ce0ab25061f918139c
Keaton <az.keaton@gmail.com>
2022-01-06 22:27:30 -0700

    begining inquirer promts

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,15 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer')
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'projectName',
+            message: '**Project Name: ',
+        }
+    ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
